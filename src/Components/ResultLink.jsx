1a4f951608975e49a9a1f3293b8558c4b2783a98
Sachin Nautiyal <nautiyalsachin217@gmail.com>
2023-11-09 07:17:11 +0530

    copy-to clipboard package added

--- src/Components/ResultLink.jsx
+++ src/Components/ResultLink.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard'; 
 
 const ResultLink = () => {
  
